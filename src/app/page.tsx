0bf870094863a9190b1a24d8dd70dbf7746efdd6
krystofpiorecky <krystofpiorecky@gmail.com>
2024-12-23 20:36:00 +0100

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,16 @@ import Hero from '@/sections/hero/hero'
 import Projects from '@/sections/projects/projects'
 import Socials from '@/sections/socials/socials'
 import Sticker from '@/sections/sticker/sticker'
+import { useEffect, useState } from 'react';
 import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
 
 export default function Home() {
-  const userAgent = navigator.userAgent;
-  const isSamsungBrowser = /SamsungBrowser/i.test(userAgent);
+  const [isSamsungBrowser, setIsSamsungBrowser] = useState(false);
+  useEffect(() => {
+    setIsSamsungBrowser(/SamsungBrowser/i.test(navigator.userAgent));
+  }, []);
+  
   return <SimpleBar style={{ maxHeight: '100vh', overflowX: 'hidden', color: "#fff" }}>
     <main className={styles.div} data-samsungbrowser={isSamsungBrowser}>
       <Hero></Hero>
